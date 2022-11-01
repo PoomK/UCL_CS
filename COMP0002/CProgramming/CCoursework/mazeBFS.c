4e60438a8dc6198abb979231a80c51c7e53f4abc
PoomK <pittawas.kijoran@gmail.com>
2022-11-01 10:28:50 +0000

    added hash table files

--- COMP0002/CProgramming/CCoursework/mazeBFS.c
+++ COMP0002/CProgramming/CCoursework/mazeBFS.c
@@ -19,7 +19,7 @@ const int moveDistance = 40;
 const int waitTime = 40;
 int triangleX[3], triangleY[3], direction, currentXGrid, currentYGrid, forwardValue;
 char pathArray[100];
-
+int xNodes[100], yNodes[100];
 
 // 2D array for the maze
 int grid[12][12] = {
