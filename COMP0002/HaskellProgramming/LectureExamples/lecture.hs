092433002cc750ff915609c8f5c32750a4897da7
PoomK <pittawas.kijoran@gmail.com>
2022-12-25 20:21:14 +0700

    updated all files

--- COMP0002/HaskellProgramming/LectureExamples/lecture.hs
+++ COMP0002/HaskellProgramming/LectureExamples/lecture.hs
@@ -10,3 +10,8 @@ insert :: Int -> [Int] -> [Int]
 insert n [] = [n]
 insert n (x:xs) = if n <= x then n:x:xs else x : insert n (xs)
 
+empty :: Q a -- an empty queue
+add :: a -> Q a -> Q a -- add element at back
+remove :: Qa -> Qa -- remove element from front
+front :: Qa -> a -- inspect front element
+isEmpty :: Qa -> Bool -- check if queue is empty
